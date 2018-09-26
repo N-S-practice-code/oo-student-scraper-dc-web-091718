d5d7a7339ce85ede6ac8e095336e07e0038cfba3
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-26 19:32:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each{|attr, value|}
+    attributes_hash.each{|attr, value| self.send("#{attr}=", value)}
   end
 
   def self.all
